6a56cde94db874fdb47032ba9f5012c47cdbf257
tbotnz <tonynealon1989@gmail.com>
2026-02-20 08:22:29 +0000

    .

--- api/main.py
+++ api/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
 
 app = FastAPI()
 
 
 @app.get("/call/{phone_number}")
 def call(phone_number: str):
+    # Response format: redirect_extension,caller_id
     print(f"Incoming call from: {phone_number}")
-    return {"status": "ok", "phone_number": phone_number}
+    if phone_number == "4431631":
+        print("Redirecting call to 777")
+        return PlainTextResponse("777,+15551234567")
+    return PlainTextResponse("888,+15559999999")
